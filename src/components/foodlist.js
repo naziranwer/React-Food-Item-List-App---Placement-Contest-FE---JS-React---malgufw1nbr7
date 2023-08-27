d2913b233229ad11107cff084e2fd3af4a6c6180
naziranwer <122814116+naziranwer@users.noreply.github.com>
2023-08-27 19:19:27 +0530

    Update foodlist.js

--- src/components/foodlist.js
+++ src/components/foodlist.js
@@ -10,12 +10,14 @@ function FoodList() {
   const [isSecondCardEnabled, setIsSecondCardEnabled] = useState(false);
   const [isFormEnabled, setIsFormEnabled] = useState(false);
 
-  const handleAddFood = () => {
-    setIsFirstCardEnabled(true);
-  };
-
   const handleFormClick = () => {
     setIsFormEnabled(true);
+  };
+
+  const handleAddFood = () => {
+    setIsFirstCardEnabled(true);
+    setIsSecondCardEnabled(true);
+    setIsFormEnabled(false); // Reset form enabled state
   };
 
   const handleSaveFood = () => {
@@ -34,63 +36,62 @@ function FoodList() {
   };
 
   const handleDeleteFood = (index) => {
-    const updatedFoods = [...foods];
-    updatedFoods.splice(index, 1);
-    setFoods(updatedFoods);
+    const newFoods = [...foods];
+    newFoods.splice(index, 1);
+    setFoods(newFoods);
   };
 
   return (
     <>
       <div className="container">
         <h1>Food Items List</h1>
-        <button
-          onClick={handleAddFood}
-          className={!isFirstCardEnabled ? "" : "hidden"}
-        >
-          Add Food
-        </button>
+        <button onClick={handleAddFood}>Add Food</button>
 
-        <div className={`card-container ${isFirstCardEnabled ? "" : "hidden"}`}>
-          <>
-            <h2>Item Name:</h2>
-            <input
-              name="itemName"
-              type="text"
-              disabled={!isFirstCardEnabled}
-              value={itemName}
-              onChange={(e) => setItemName(e.target.value)}
-            />
-            <h2>Food Type:</h2>
-            <input
-              name="foodType"
-              type="text"
-              disabled={!isFirstCardEnabled}
-              value={foodType}
-              onChange={(e) => setFoodType(e.target.value)}
-            />
-            <div
-              className={`card ${isFormEnabled ? "" : "disabled"}`}
-              onClick={handleFormClick}
-              style={{ opacity: isFormEnabled ? 1 : 0.5 }} // Set opacity based on isFormEnabled
-            >
-              <form>
-                <h2>Spiciness Level:</h2>
-                <input
-                  name="spicinessLevel"
-                  type="text"
-                  disabled={!isFormEnabled}
-                  value={spicinessLevel}
-                  onChange={(e) => setSpicinessLevel(e.target.value)}
-                />
-              </form>
-            </div>
-          </>
+        <div className="card-container">
+          {isFirstCardEnabled ? (
+            <>
+              <h2>Item Name:</h2>
+              <input
+                name="itemName"
+                type="text"
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
+                disabled={!isFirstCardEnabled}
+              />
+              <h2>Food Type:</h2>
+              <input
+                name="foodType"
+                type="text"
+                value={foodType}
+                onChange={(e) => setFoodType(e.target.value)}
+                disabled={!isFirstCardEnabled}
+              />
+              <div className={`card ${isFormEnabled ? "" : "disabled"}`}>
+                <form
+                  style={{ opacity: isFormEnabled ? 1 : 0.5 }}
+                  onSubmit={(e) => e.preventDefault()}
+                  onClick={handleFormClick}
+                >
+                  <h2>Spiciness Level:</h2>
+                  <input
+                    name="spicinessLevel"
+                    type="text"
+                    value={spicinessLevel}
+                    disabled={!isFormEnabled}
+                    onChange={(e) => setSpicinessLevel(e.target.value)}
+                  />
+                </form>
+              </div>
+            </>
+          ) : null}
         </div>
-        <div className={`card ${isSecondCardEnabled ? "" : "disabled"}`}>
-          <button onClick={handleSaveFood}>Save</button>
-        </div>
+        {isFirstCardEnabled && isFormEnabled ? (
+          <div className={`card ${isSecondCardEnabled ? "" : "disabled"}`}>
+            <button onClick={handleSaveFood}>Save</button>
+          </div>
+        ) : null}
 
-        <ul className={`list ${foods.length > 0 ? "" : "hidden"}`}>
+        <ul className="list">
           {foods.map((food, index) => (
             <li key={index}>
               {food.itemName} ({food.foodType}) - Spiciness Level:{" "}
